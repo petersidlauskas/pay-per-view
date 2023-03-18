4d077764301371d36978d9de2917ba36f1efa833
Peter Sidlauskas <peter@bronze56k.com>
2023-03-18 09:46:28 -0400

    adding files

---
+++ main.js
@@ -0,0 +1,153 @@
+
+       var dt = new Date();
+document.getElementById("date").innerHTML = dt.toLocaleTimeString();
+
+        
+
+        let thrasher = document.getElementById("thrasher");
+        let halfcab = document.getElementById("halfcab");
+        let elles = document.getElementById("elles");
+        let jordan = document.getElementById("jordan");
+        let modern = document.getElementById("modern");
+        let safari = document.getElementById("safari");
+        let alexis = document.getElementById("alexis");
+        let type71 = document.getElementById("type71");
+        let type72 = document.getElementById("type72");
+        let type73 = document.getElementById("type73");
+        let fwiw = document.getElementById("fwiw");
+        let glory = document.getElementById("glory");
+        let onlyny = document.getElementById("onlyny");
+        let revenge = document.getElementById("revenge");
+        let iverson = document.getElementById("iverson");
+        let kohler = document.getElementById("kohler");
+        let crailtap = document.getElementById("crailtap");
+        let slowtide = document.getElementById("slowtide");
+        let freja = document.getElementById("freja");
+        let csc = document.getElementById("csc");
+        let untrans = document.getElementById("untrans");
+        let loveis = document.getElementById("loveis");
+        let onto = document.getElementById("onto");
+        let mellow = document.getElementById("mellow");
+        
+
+        halfcab.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/pVbxm3ZJ5yk' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        thrasher.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/IWfEcckWtB8' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        elles.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/j37lDRBlFGU' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        jordan.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/rlsOHY4BZ1k' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        modern.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/CtkajwhHf_U' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        safari.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/2tBu-jLFgUU' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        alexis.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/xzEJdRSxH2E' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        type71.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/aRQ9i__Daxc' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        type72.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/zTp41qZRhTY' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        type73.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/100GRHueICI' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        fwiw.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://player.vimeo.com/video/314351115?h=4eb22c06de' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        glory.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/ddtdB4ccpvM' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        onlyny.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://player.vimeo.com/video/580381592?muted=0' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        revenge.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/pbou7y4Tzso' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        iverson.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://youtube.com/shorts/-bvoI_Ck-0U?feature=share' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        kohler.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/LW5WNjR6tHo' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        crailtap.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/q8e7hwLTsLo' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        slowtide.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://player.vimeo.com/video/190595693?h=325a990c32' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        freja.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/W79AcO_pTwc' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        csc.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://www.youtube.com/embed/x5eRWfUofko' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        untrans.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://player.vimeo.com/video/676660108?h=753005e47d' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        loveis.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://player.vimeo.com/video/677295085?h=5a8ccb0e56' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        onto.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://player.vimeo.com/video/247010285?h=f14eb15a29' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+        mellow.addEventListener("click", function(){
+            
+            topvid.innerHTML = "<iframe width='700' height='400' src='https://player.vimeo.com/video/136782450?h=5a29d3c3ea&color=ffffff&title=0&byline=0&portrait=0' frameborder='0' allowfullscreen=''></iframe>"
+        })
+
+   
